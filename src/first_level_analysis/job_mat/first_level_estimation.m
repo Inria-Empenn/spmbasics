21688c604db80e561afc2e2ae2fce876067d7d08
Melvin S. Atay <atay.m.selim@gmail.com>
2024-04-18 11:34:18 +0200

    ignore editi

--- src/first_level_analysis/job_mat/first_level_estimation.m
+++
@@ -1,8 +0,0 @@
-%-----------------------------------------------------------------------
-% Job saved on 17-Apr-2024 17:00:41 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7771)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.fmri_est.spmmat = {'/home/matay/spmbasics/src/first_level_analysis/SPM.mat'};
-matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
