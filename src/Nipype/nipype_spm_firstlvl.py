116ebe68ba720ba39d9bb66d07ce783716481a28
Melvin S. Atay <atay.m.selim@gmail.com>
2024-05-03 19:00:50 +0200

    initial first level code

---
+++ src/Nipype/nipype_spm_firstlvl.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from nilearn import plotting
+
+import os
+import json
+from nipype.interfaces.spm import Level1Design, EstimateModel, EstimateContrast
+from nipype.algorithms.modelgen import SpecifySPMModel
+from nipype.interfaces.utility import Function, IdentityInterface
+from nipype.interfaces.io import SelectFiles, DataSink
+from nipype import Workflow, Node
+from nipype.interfaces import spm
+from nipype.interfaces import fsl
+from nipype.interfaces import matlab as mlab
+
+
+spm.SPMCommand.set_mlab_paths(paths=os.path.abspath('./Documents/MATLAB/spm12/'), matlab_cmd='/soft/matlab_hd/R2020b/bin/glnxa64/MATLAB -nodesktop -nosplash')
+
+
+mlab.MatlabCommand.set_default_matlab_cmd("/soft/matlab_hd/R2020b/bin/glnxa64/MATLAB  -nodesktop -nosplash")
+mlab.MatlabCommand.set_default_paths(os.path.abspath('./Documents/MATLAB/spm12/'))
+
+
+
+spm.SPMCommand().version
+
+
+fsl.FSLCommand.set_default_output_type('NIFTI')
+
+
+preproc = Workflow(name='block_1stlvl', base_dir=os.path.abspath('./spmbasics/data/output'))
+
+
+base_dir = os.path.join(os.environ['HOME'], 'spmbasics/data/')
+
+
+experiment_dir = os.path.join(base_dir, 'output')
+output_dir = 'datasink'
+working_dir = 'workingdir'
+
+# list of subject identifiers
+subject_list = ['01']
+
+# TR of functional images
+with open(os.path.join(base_dir, 'MoAEpilot/task-auditory_bold.json'), 'rt') as fp:
+    task_info = json.load(fp)
+TR = task_info['RepetitionTime']
+
+# Smoothing width used during preprocessing
+fwhm = 6
+
+
+
+# SpecifyModel - Generates SPM-specific Model
+modelspec = Node(SpecifySPMModel(concatenate_runs=False,
+                                 input_units='scans',
+                                 output_units='scans',
+                                 time_repetition=TR,
+                                 high_pass_filter_cutoff=128),
+                 name="modelspec")
+
+# Level1Design - Generates an SPM design matrix
+level1design = Node(Level1Design(bases={'hrf': {'derivs': [0, 0]}},
+                                 timing_units='scans',
+                                 interscan_interval=TR,
+                                 volterra_expansion_order=1,
+                                 flags={'mthresh': 0.8},
+                                 global_intensity_normalization='none',
+                                 microtime_onset=8,
+                                 microtime_resolution=16,
+                                 model_serial_correlations='AR(1)'),
+                    name="level1design")
+
+# EstimateModel - estimate the parameters of the model
+level1estimate = Node(EstimateModel(estimation_method={'Classical': 1}),
+                      write_residuals=False, 
+                      name="level1estimate")
+
+# EstimateContrast - estimates contrasts
+level1conest = Node(EstimateContrast(), name="level1conest")
+
+
+# Condition names
+condition_names = ['listening']
+#onsets = [6,18, 30, 42, 54, 66, 78]
+# Contrasts
+#onsets = ['listening',      'T', condition_names, [6, 18, 30, 42, 54, 66, 78]
+cont01 = ['listening > rest','T', condition_names, [1, 0]]
+
+contrast_list = [cont01]
+
+
+
+
+for group in trialinfo.groupby('trial_type'):
+    print(group)
+    print("")
+
+
+def subjectinfo(subject_id):
+
+    import pandas as pd
+    from nipype.interfaces.base import Bunch
+    
+    trialinfo = pd.read_table(os.path.join(base_dir, 'MoAEpilot/sub-01/func/sub-01_task-auditory_events.tsv'))
+    trialinfo.head()
+    conditions = []
+    onsets = []
+    durations = []
+
+    for group in trialinfo.groupby('trial_type'):
+        conditions.append(group[0])
+        onsets.append(list(group[1].onset - 10)) # subtracting 10s due to removing of 4 dummy scans
+        durations.append(group[1].duration.tolist())
+
+    subject_info = [Bunch(conditions=conditions,
+                          onsets=onsets,
+                          durations=durations,
+                          #amplitudes=None,
+                          tmod=None,
+                          pmod=None,
+                          #regressor_names=None,
+                          #regressors=None
+                         )]
+
+    return subject_info  # this output will later be returned to infosource
+
+# Get Subject Info - get subject specific condition information
+getsubjectinfo = Node(Function(input_names=['subject_id'],
+                               output_names=['subject_info'],
+                               function=subjectinfo),
+                      name='getsubjectinfo')
+
+
+# Infosource - a function free node to iterate over the list of subject names
+infosource = Node(IdentityInterface(fields=['subject_id',
+                                            'fwhm_id',
+                                            'contrasts'],
+                                    contrasts=contrast_list),
+                  name="infosource")
+infosource.iterables = [('subject_id', subject_list),
+                        ('fwhm_id', fwhm)]
